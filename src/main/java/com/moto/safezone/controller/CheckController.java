dc1b5172f50a8948296888fffd2f1dd0c7fb500d
Daniel Osypenko (Partner) <Daniel.Osypenko@motorolasolutions.com>
2020-07-30 11:11:20 +0300

    cors problem check

--- src/main/java/com/moto/safezone/controller/CheckController.java
+++ src/main/java/com/moto/safezone/controller/CheckController.java
@@ -1,8 +1,10 @@
 package com.moto.safezone.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*", allowCredentials = "true")
 @RestController
 public class CheckController {
 
@@ -11,8 +13,15 @@ public class CheckController {
 	public String getHello() {
 		return "hello world";
 	}
+
 	@GetMapping("/res")
 	public String getRestricted() {
-		return "restrincted";
+		return "restrincted get";
 	}
+
+	@GetMapping("/res-pos")
+	public String getRestrictedPost() {
+		return "restrincted post";
+	}
+
 }
